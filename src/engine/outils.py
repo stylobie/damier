d88149d4086af0efd60afa0aa32b75dee4a1ba59
Catalin VASCU <catalin.vascu@salviadeveloppement.com>
2018-04-24 02:20:35 +0200

    #

--- src/engine/outils.py
+++ src/engine/outils.py
@@ -6,3 +6,10 @@ class Outils :
     @staticmethod
     def estPair(n) :
         return n % 2 == 0
+
+    @staticmethod
+    def getIndent(indent):
+        if indent < 1 :
+            return ""
+        else :
+            return ("{:>"+"{:d}".format(indent)+"}").format(" ")
